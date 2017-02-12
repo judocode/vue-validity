f0ca54f2765923d27d7f85a192d19d30ac623139
Justus <jrenton87@gmail.com>
2017-02-11 21:50:25 -0600

    add docs build

--- build/docs.js
+++ build/docs.js
@@ -9,36 +9,10 @@ var chalk = require('chalk')
 var shell = require('shelljs')
 var webpack = require('webpack')
 var config = require('../config')
-var webpackConfig = require('./webpack.prod.conf')
 var webpackDocsConfig = require('./webpack.docs.conf')
 
 var spinner = ora('building for production...')
 spinner.start()
-
-var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
-shell.rm('-rf', assetsPath)
-shell.mkdir('-p', assetsPath)
-shell.config.silent = true
-shell.cp('-R', 'static/*', assetsPath)
-shell.config.silent = false
-
-webpack(webpackConfig, function (err, stats) {
-  spinner.stop()
-  if (err) throw err
-  process.stdout.write(stats.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false
-  }) + '\n\n')
-
-  console.log(chalk.cyan('  Build complete.\n'))
-  console.log(chalk.yellow(
-    '  Tip: built files are meant to be served over an HTTP server.\n' +
-    '  Opening index.html over file:// won\'t work.\n'
-  ))
-})
 
 var assetsPath = path.join(config.docs.assetsRoot, config.docs.assetsSubDirectory)
 shell.rm('-rf', assetsPath)
@@ -59,9 +33,5 @@ webpack(webpackDocsConfig, function (err, stats) {
   }) + '\n\n')
 
   console.log(chalk.cyan('  Build complete.\n'))
-  console.log(chalk.yellow(
-    '  Tip: built files are meant to be served over an HTTP server.\n' +
-    '  Opening index.html over file:// won\'t work.\n'
-  ))
 })
 
